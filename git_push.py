13b05779add87b0e417e0abd47c09add21fc7a44
Aaron Huang <ahuang25@andover.edu>
2025-03-29 16:13:48 -0400

    add image class: implement image url

--- git_push.py
+++ git_push.py
@@ -1,5 +1,11 @@
 class Italian_Brainrot:
-    def __init__(self, name, image):
+    def __init__(self, name, image, image_url):
         self.name = name
-        self.image = image
-    
+        self.image = Image(image_url)
+
+class Image:
+    def __init__(self, url):
+        self.url = url
+
+    def __str__(self):
+        return f"Image URL: {self.url}"
